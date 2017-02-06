c265c9e3ed5a6949ea3dc6eec7e83797f81e4ed1
Bengt Kleberg <qbengt@qoorp.com>
2017-02-06 10:47:16 +0100

    Save startxref in Reader
    The location of the last xref structure is needed when saving a new
    PDF. It should be in the trailer dict as /Prev.
    
    Here it is used when reading the PDF and thus available. It only needs to
    be saved. Accessing it can done from another place, later.

--- read.go
+++ read.go
@@ -79,6 +79,7 @@ import (
 type Reader struct {
 	f          io.ReaderAt
 	end        int64
+	startxref  int64
 	xref       []xref
 	trailer    dict
 	trailerptr objptr
@@ -161,6 +162,7 @@ func NewReaderEncrypted(f io.ReaderAt, size int64, pw func() string) (*Reader, e
 	if err != nil {
 		return nil, err
 	}
+	r.startxref = startxref
 	r.xref = xref
 	r.trailer = trailer
 	r.trailerptr = trailerptr
