9cb03025cecd36144a4732a1d089eaa07828739c
Gareth Watts <gareth@omnipotent.net>
2017-03-25 22:38:32 -0700

    Make PDF header parser more tolerant
    Not all PDF content producers strictly adhere to the spec recommendations.

--- read.go
+++ read.go
@@ -124,9 +124,11 @@ func NewReader(f io.ReaderAt, size int64) (*Reader, error) {
 func NewReaderEncrypted(f io.ReaderAt, size int64, pw func() string) (*Reader, error) {
 	buf := make([]byte, 10)
 	f.ReadAt(buf, 0)
-	if !bytes.HasPrefix(buf, []byte("%PDF-1.")) || buf[7] < '0' || buf[7] > '7' || buf[8] != '\r' && buf[8] != '\n' {
+
+	if !bytes.HasPrefix(buf, []byte("%PDF-1.")) {
 		return nil, fmt.Errorf("not a PDF file: invalid header")
 	}
+
 	end := size
 	const endChunk = 100
 	buf = make([]byte, endChunk)
